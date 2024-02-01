9a7c6990e446d91795b89de9800ac4df7b5e57ea
Maduro29 <manhdungtran.vnuuet@gmail.com>
2024-02-02 00:54:31 +0700

    update port in index.js

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.get("/", function (request, response) {
 });
 
 // Bắt đầu máy chủ
-app.listen(3000, function () {
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
   console.log("Server listening on port 3000");
 });
